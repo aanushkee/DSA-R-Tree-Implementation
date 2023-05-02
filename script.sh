ab8f4421ed3e3380a7c672db42b38fc27972e985
anushka patil <87687044+aanushkee@users.noreply.github.com>
2023-05-02 21:52:31 +0545

    create and update script.sh

---
+++ script.sh
@@ -0,0 +1,5 @@
+gcc -c finalproject.c
+gcc -o exe finalproject.c
+./exe
+
+#To run the file just type sh script.sh on the terminal if you're using a Mac or just copy paste the above code if you're using Windows
